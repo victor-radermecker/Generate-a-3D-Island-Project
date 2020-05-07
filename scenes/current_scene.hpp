1eff10653023b3860c16b3ea8272e236301fc5b3
Vincent Fournet <vincent.fournet@polytechnique.edu>
2020-05-07 09:51:23 +0200

    Base

--- scenes/current_scene.hpp
+++ scenes/current_scene.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 // Indicate here which exercise should be currently considered using its define keyword
-#define SCENE_DEFAULT_3D_GRAPHICS // replace this keyword to change the scene
+#define SCENE_3D_GRAPHICS // replace this keyword to change the scene
 
 // Help: The keywords associated to other scenes are listed below
 //
